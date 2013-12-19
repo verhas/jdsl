9a2cc888858f06f6551f0034126acc233a53ea10
Peter Verhas <peter@verhas.com>
2013-12-19 22:17:16 +0100

    new class

---
+++ src/main/java/com/javax0/jdsl/analyzers/NoExecutorListAnalyzer.java
@@ -0,0 +1,15 @@
+package com.javax0.jdsl.analyzers;
+
+import java.util.List;
+
+public class NoExecutorListAnalyzer extends ListAnalyzer {
+
+	public NoExecutorListAnalyzer() {
+		super(null);
+	}
+
+	@Override
+	public List<Analyzer> getAnalyzerList() {
+		return super.getAnalyzerList();
+	}
+}
