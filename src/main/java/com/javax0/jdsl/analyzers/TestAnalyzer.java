1bc104e229d29008de6e6101446df394d5d15c76
Peter Verhas <peter@verhas.com>
2014-01-23 23:21:08 +0100

    New analyzer to test syntax (look ahead)

---
+++ src/main/java/com/javax0/jdsl/analyzers/TestAnalyzer.java
@@ -0,0 +1,63 @@
+package com.javax0.jdsl.analyzers;
+
+import com.javax0.jdsl.GrammarDefinition;
+
+/**
+ * An analyzer that can be used to test that an analyzer would be successful at
+ * a certain point.
+ * <p>
+ * The test will result success/failed if the underlying analyzer is successful
+ * and fails if that fail. The actual return success/fail is inverted if the
+ * constructor argument {@code invert} is true. When the returned result is
+ * success it contains the original source code and {@code null} state and
+ * executor.
+ * <p>
+ * To use this analyzer statically import the method {@link #test(Analyzer)}
+ * and/or {@link #not(Analyzer)} and use them in the {@link GrammarDefinition}
+ * implementation.
+ * <p>
+ * The method {@link #testNot(Analyzer)} is a complimentary method and can be
+ * used instead of {@link #not(Analyzer)} when the word "testNot" is better for
+ * the readability.
+ * 
+ * @author Peter Verhas
+ * 
+ */
+public class TestAnalyzer implements Rule {
+
+	private final Analyzer analyzerToTest;
+	private final boolean invert;
+
+	private TestAnalyzer(final Analyzer analyzerToTest, final boolean invert) {
+		this.analyzerToTest = analyzerToTest;
+		this.invert = invert;
+	}
+
+	public static Rule test(final Analyzer analyzerToTest) {
+		return new TestAnalyzer(analyzerToTest, false);
+	}
+
+	public static Rule testNot(final Analyzer analyzerToTest) {
+		return not(analyzerToTest);
+	}
+
+	public static Rule not(final Analyzer analyzerToTest) {
+		return new TestAnalyzer(analyzerToTest, true);
+	}
+
+	@Override
+	public AnalysisResult analyze(final SourceCode input) {
+		final AnalysisResult result = analyzerToTest.analyze(input);
+		final boolean success = invert ? !result.wasSuccessful() : result
+				.wasSuccessful();
+		if (success) {
+			return SimpleAnalysisResult.success(TestAnalyzer.class, input,
+					null, null);
+		} else {
+			return SimpleAnalysisResult.failed(TestAnalyzer.class,
+					"underlying analyzer "
+							+ analyzerToTest.getClass().getName()
+							+ " was successful");
+		}
+	}
+}
---
+++ src/test/java/com/javax0/jdsl/analyzers/TestAnalyzerTest.java
@@ -0,0 +1,108 @@
+package com.javax0.jdsl.analyzers;
+
+import static com.javax0.jdsl.analyzers.TestAnalyzer.not;
+import static com.javax0.jdsl.analyzers.TestAnalyzer.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.javax0.jdsl.executors.SimpleListExecutor;
+
+public class TestAnalyzerTest {
+
+	@SuppressWarnings("unused")
+	@Test
+	public void given_AFailingUnderlyingAnalyzer_when_CallingTestAnalyzer_then_TestAnalyzerAlsoFails() {
+		final Analyzer failingAnalyzer;
+		final Rule testAnalyzer;
+		GIVEN: {
+			testAnalyzer = test(new Analyzer() {
+				@Override
+				public AnalysisResult analyze(final SourceCode input) {
+					return SimpleAnalysisResult.failed(null);
+				}
+			});
+		}
+		final AnalysisResult result;
+		WHEN: {
+			result = testAnalyzer.analyze(null);
+		}
+		THEN: {
+			Assert.assertFalse(result.wasSuccessful());
+		}
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	public void given_ASuccessfulUnderlyingAnalyzer_when_CallingTestAnalyzer_then_TestAnalyzerAlsoSuccessfulAndHasNullExecutorAndNullState() {
+		final Analyzer failingAnalyzer;
+		final Rule testAnalyzer;
+		GIVEN: {
+			testAnalyzer = test(new Analyzer() {
+				@Override
+				public AnalysisResult analyze(final SourceCode input) {
+					return SimpleAnalysisResult.success(null, null,
+							new SimpleListExecutor(), new AnalysisState() {
+							});
+				}
+			});
+		}
+		final AnalysisResult result;
+		WHEN: {
+			result = testAnalyzer.analyze(null);
+		}
+		THEN: {
+			Assert.assertTrue(result.wasSuccessful());
+			Assert.assertNull(result.getState());
+			Assert.assertNull(result.getExecutor());
+		}
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	public void given_AFailingUnderlyingAnalyzer_when_CallingTestNotAnalyzer_then_TestAnalyzerAlsoSuccessfulAndHasNullExecutorAndNullState() {
+		final Analyzer failingAnalyzer;
+		final Rule testAnalyzer;
+		GIVEN: {
+			testAnalyzer = not(new Analyzer() {
+				@Override
+				public AnalysisResult analyze(final SourceCode input) {
+					return SimpleAnalysisResult.failed(null);
+				}
+			});
+		}
+		final AnalysisResult result;
+		WHEN: {
+			result = testAnalyzer.analyze(null);
+		}
+		THEN: {
+			Assert.assertTrue(result.wasSuccessful());
+			Assert.assertNull(result.getState());
+			Assert.assertNull(result.getExecutor());
+		}
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	public void given_ASuccessfulUnderlyingAnalyzer_when_CallingTestNotAnalyzer_then_TestAnalyzerFails() {
+		final Analyzer failingAnalyzer;
+		final Rule testAnalyzer;
+		GIVEN: {
+			testAnalyzer = not(new Analyzer() {
+				@Override
+				public AnalysisResult analyze(final SourceCode input) {
+					return SimpleAnalysisResult.success(null, null,
+							new SimpleListExecutor(), new AnalysisState() {
+							});
+				}
+			});
+		}
+		final AnalysisResult result;
+		WHEN: {
+			result = testAnalyzer.analyze(null);
+		}
+		THEN: {
+			Assert.assertFalse(result.wasSuccessful());
+		}
+	}
+}
