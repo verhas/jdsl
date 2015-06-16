a658dca39b9996d58230067294fcb4566f07390b
Michael1993 <misi.verhas@gmail.com>
2015-06-16 21:02:26 +0200

    simplified junit tests to make them more readable

--- src/test/java/com/javax0/jdsl/analyzers/TestAnalyzerTest.java
+++ src/test/java/com/javax0/jdsl/analyzers/TestAnalyzerTest.java
@@ -1,108 +1,54 @@
 package com.javax0.jdsl.analyzers;
 
+
+import static com.javax0.jdsl.analyzers.MockAnalyzerGeneratorUtil.newFailingAnalyzer;
+import static com.javax0.jdsl.analyzers.MockAnalyzerGeneratorUtil.newSuccessfulAnalyzer;
 import static com.javax0.jdsl.analyzers.TestAnalyzer.not;
 import static com.javax0.jdsl.analyzers.TestAnalyzer.test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Test;
-
-import com.javax0.jdsl.executors.SimpleListExecutor;
 
 public class TestAnalyzerTest {
 
-	@SuppressWarnings("unused")
 	@Test
-	public void given_AFailingUnderlyingAnalyzer_when_CallingTestAnalyzer_then_TestAnalyzerAlsoFails() {
-		final Analyzer failingAnalyzer;
-		final Rule testAnalyzer;
-		GIVEN: {
-			testAnalyzer = test(new Analyzer() {
-				@Override
-				public AnalysisResult analyze(final SourceCode input) {
-					return SimpleAnalysisResult.failed(null);
-				}
-			});
-		}
-		final AnalysisResult result;
-		WHEN: {
-			result = testAnalyzer.analyze(null);
-		}
-		THEN: {
-			Assert.assertFalse(result.wasSuccessful());
-		}
+	public void failsWhenUnderlyingAnalyzerFails() {
+		assertFalse(test(newFailingAnalyzer()).analyze(null).wasSuccessful());
 	}
-
-	@SuppressWarnings("unused")
 	@Test
-	public void given_ASuccessfulUnderlyingAnalyzer_when_CallingTestAnalyzer_then_TestAnalyzerAlsoSuccessfulAndHasNullExecutorAndNullState() {
-		final Analyzer failingAnalyzer;
-		final Rule testAnalyzer;
-		GIVEN: {
-			testAnalyzer = test(new Analyzer() {
-				@Override
-				public AnalysisResult analyze(final SourceCode input) {
-					return SimpleAnalysisResult.success(null, null,
-							new SimpleListExecutor(), new State() {
-							});
-				}
-			});
-		}
-		final AnalysisResult result;
-		WHEN: {
-			result = testAnalyzer.analyze(null);
-		}
-		THEN: {
-			Assert.assertTrue(result.wasSuccessful());
-			Assert.assertNull(result.getState());
-			Assert.assertNull(result.getExecutor());
-		}
+	public void failsWhenNegatedUnderlyingAnalyzerIsSuccessful() {
+		assertFalse(not(newSuccessfulAnalyzer()).analyze(null).wasSuccessful());
 	}
-
-	@SuppressWarnings("unused")
+	
 	@Test
-	public void given_AFailingUnderlyingAnalyzer_when_CallingTestNotAnalyzer_then_TestAnalyzerAlsoSuccessfulAndHasNullExecutorAndNullState() {
-		final Analyzer failingAnalyzer;
-		final Rule testAnalyzer;
-		GIVEN: {
-			testAnalyzer = not(new Analyzer() {
-				@Override
-				public AnalysisResult analyze(final SourceCode input) {
-					return SimpleAnalysisResult.failed(null);
-				}
-			});
-		}
-		final AnalysisResult result;
-		WHEN: {
-			result = testAnalyzer.analyze(null);
-		}
-		THEN: {
-			Assert.assertTrue(result.wasSuccessful());
-			Assert.assertNull(result.getState());
-			Assert.assertNull(result.getExecutor());
-		}
+	public void isSuccessfulWhenUnderlyingAnalyzerIsSuccessful() {		
+		assertTrue(test(newSuccessfulAnalyzer()).analyze(null).wasSuccessful());
 	}
-
-	@SuppressWarnings("unused")
+	
 	@Test
-	public void given_ASuccessfulUnderlyingAnalyzer_when_CallingTestNotAnalyzer_then_TestAnalyzerFails() {
-		final Analyzer failingAnalyzer;
-		final Rule testAnalyzer;
-		GIVEN: {
-			testAnalyzer = not(new Analyzer() {
-				@Override
-				public AnalysisResult analyze(final SourceCode input) {
-					return SimpleAnalysisResult.success(null, null,
-							new SimpleListExecutor(), new State() {
-							});
-				}
-			});
-		}
-		final AnalysisResult result;
-		WHEN: {
-			result = testAnalyzer.analyze(null);
-		}
-		THEN: {
-			Assert.assertFalse(result.wasSuccessful());
-		}
+	public void hasNullStateWhenUnderlyingAnalyzerIsSuccessful() {		
+		assertNull(test(newSuccessfulAnalyzer()).analyze(null).getState());
+	}
+	
+	@Test
+	public void hasNullExecutorWhenUnderlyingAnalyzerIsSuccessful() {				
+		assertNull(test(newSuccessfulAnalyzer()).analyze(null).getExecutor());
+	}
+	
+	@Test
+	public void isSuccessfulWhenNegatedUnderlyingAnalyzerFails() {		
+		assertTrue(not(newFailingAnalyzer()).analyze(null).wasSuccessful());
+	}
+	
+	@Test
+	public void hasNullStateWhenNegatedUnderlyingAnalyzerFails() {		
+		assertNull(not(newFailingAnalyzer()).analyze(null).getState());
+	}
+	
+	@Test
+	public void hasNullExecutorWhenNegatedUnderlyingAnalyzerFails() {				
+		assertNull(not(newFailingAnalyzer()).analyze(null).getExecutor());
 	}
 }
